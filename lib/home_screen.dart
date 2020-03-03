5208bf87bdc20a2ed9b1546dabbe25be093aba71
Joshua Leger <legermjoshua@gmail.com>
2020-03-03 08:33:54 -0800

    Added bottom bar to home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,8 +9,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(title: Text(_connection.username)),
+      appBar: AppBar(
+        title: Text(_connection.username),
+      ),
       body: Center(child: Text('Welcome ' + _connection.username)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text('Friends'),
+            icon: Icon(Icons.supervised_user_circle),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Games'),
+            icon: Icon(Icons.videogame_asset),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Notifications'),
+            icon: Icon(Icons.notifications),
+          ),
+        ],
+      ),
     );
   }
 }
